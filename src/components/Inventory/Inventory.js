6daa11d7115ff6cc32c113900b492b2d8bbc7df8
ab-alamin <abdullahalamin9876@gmail.com>
2021-10-06 23:19:38 +0600

    updated'

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h2>This is inventory</h2>
+            <h2>This is inventory 
+                <p>how are you</p>
+            </h2>
         </div>
     );
 };
